25f918a57f6358cc1a8aa23dc87bb99ec5fa24b9
yogeshdhameliya6013 <yogesh.dhameliya@outlook.com>
2023-05-10 14:19:30 +0530

    update

--- blindauction.py
+++ blindauction.py
@@ -3,27 +3,33 @@ next = True
 auction ={}
 auction_list =[]
 record_no =0
-    
+bid_counter = 0
     
 while next :
-    name= input("what is your name : ")
-    bid_amount = int(input("what's your bid : "))
-    auction["name"] = name
-    auction["bid_amount"] = bid_amount
-    auction["record"] =record_no
-    auction_list = auction
-    print(auction)
-    print(auction_list)
+   
     bidder =input("are there any other bidders ? type yes=y or no=n : ").lower()
+    
 
     if bidder == "n":
+        bio_counter = 0
         next = False
     elif bidder == "y" :
-        record_no = record_no+1
-        auction["name"] = name
-        auction["bid_amount"] = bid_amount
-        auction["record"] =record_no
-        auction_list.append(auction)
-        print(auction)
+        bio_counter = 1
+        name= input("what is your name : ")
+        bid_amount = int(input("what's your bid : "))
+        auction_len = len(auction_list) +1
+        print(auction_len)
+        
+        #auction_list[auction_len]["name"] = name
+        #auction_list[auction_len]["bid_amount"] = bid_amount
+
+        auction_list[0]["name"] = name
+        auction_list[0]["bid_amount"] = bid_amount
+
         print(auction_list)
+        auction_len = auction_len + 1
+
+print(auction_list)
+
+       
 
