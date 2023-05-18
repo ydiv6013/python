b32591fed46d9151035fec745d53af41cc073e07
yogeshdhameliya6013 <yogesh.dhameliya@outlook.com>
2023-05-18 17:15:32 +0530

    Class inheritance

--- classinheritance.py
+++ classinheritance.py
@@ -1,6 +1,8 @@
 # A Python program to demonstrate inheritance
 
 class Person :
+
+    master_key ="6013"
     #constructor
     def __init__(self ,name,id):
         self.name = name
@@ -24,6 +26,9 @@ class Employee (Person):
         self.salary =salary
         self.post =post
 
+    def getSalary(self):
+        return self.salary
+    
     def display(self):
         super().display()
         print(self.salary)
@@ -32,7 +37,55 @@ class Employee (Person):
     def isEmployee(self):
         return True
 
+class Student(Employee):
+    def __init__(self, name, id, salary, post,age,dob):
+        super().__init__(name, id, salary, post)
+        self.age =age
+        self.dob = dob
 
+    def getAge(self):
+        return self.age
+    
+    def getDob(self):
+        return self.dob
+    
+    def display(self):
+        super().display()
+        print(self.age)
+        print(self.dob)
+
+class Mother():
+    def __init__(self , name , age):
+        self.name = name
+        self.age = age
+
+    def getName(self):
+        return self.name
+    def getAge(self):
+        return self.age
+    
+    def display(self):
+        print(f"mother's name is {self.name} and her age is {self.age}")
+        
+class Father():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def getName(self):
+        return self.name
+    def getAge(self):
+        return self.age
+    
+    def display(self):
+        print(f"father's name is {self.name} and his age is {self.age}")
+
+
+class Parents(Father,Mother):
+    def __init__(self,name,age):
+        super().__init__(name,age)
+    
+    
 person = Person("Yogesh",102)
 print(person.getName(),person.getId(),person.isEmployee())
 person.display()
@@ -40,3 +93,16 @@ person.display()
 employee =Employee("Hiren",103,15000,"Sales Manager")
 print(employee.getName(),employee.getId(),employee.isEmployee())
 employee.display()
+print(f"salary of {employee.getName()} is {employee.getSalary()}")
+
+student = Student("Mohit",104,20000,"Manager",26,"05/05/1993")
+student.display()
+print(f"salary of {student.getName()} is {student.getSalary()}")
+print(f"Dob of {student.getName()} is {student.getDob()}")
+print(f"Age of {student.getName()} is {student.getAge()}")
+print(student.master_key)
+father = Father("Rameshbhai",58)
+mother = Mother("Devkuvarben" , 59)
+parents = Parents("Hina",56)
+mother.display()
+father.display()
