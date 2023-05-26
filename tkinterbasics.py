228f58e6029711318c68492c16a0b9b84216db80
yogeshdhameliya6013 <yogesh.dhameliya@outlook.com>
2023-05-26 10:30:22 +0530

    tkinter

---
+++ tkinterbasics.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# TK() will initialize the window
+window = tk.Tk()
+
+window.title("Python GUI Program")
+window.minsize(700,700)
+
+
+
+
+# It keeps window remains open
+window.mainloop()
